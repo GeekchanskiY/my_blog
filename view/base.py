ba52c83a5d3dd98aa9e9b0b0bb506ffda2017473
geekchanskiy <astrovskiy.04@gmail.com>
2025-10-03 08:52:47 +0300

    update ui

--- view/base.py
+++ view/base.py
@@ -9,7 +9,7 @@ base_bp = Blueprint('base', __name__)
 @base_bp.route("/", methods=['GET'])
 def index():
     page = request.args.get('page', 1, type=int)
-    per_page = request.args.get('per_page', 2, type=int)
+    per_page = request.args.get('per_page', 4, type=int)
 
     data, has_next = controller.index(page=page, per_page=per_page)
     
